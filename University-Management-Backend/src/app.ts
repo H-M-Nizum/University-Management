cbcea57cd6560ea76702e9a6d60b314ea0f168bc
H-M-Nizum <hmnizum1714032@gmail.com>
2025-03-02 01:14:23 +0600

    Project setup

---
+++ University-Management-Backend/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+// import { studentRoutes } from './app/modules/student/student.route';
+import cors from 'cors';
+
+const app: Application = express();
+
+// Parser
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
